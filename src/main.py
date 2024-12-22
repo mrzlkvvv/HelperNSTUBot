91763b2ce624fb0f315e043de5683bb22b2e9a6a
Kirill Merzlyakov <kiri11.mer31yak0v@gmail.com>
2024-12-22 19:03:28 +0700

    Init

---
+++ src/main.py
@@ -0,0 +1,29 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+import config
+
+
+bot = Bot(token=config.TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer('Hello!')
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        exit(0)
